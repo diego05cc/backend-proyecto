5bbd1cd9699201d3bed4815cb8088a90f9b2421a
diego05 <diego05@DIEGO05CC>
2024-09-24 11:29:37 -0500

    Agregar archivos de proyecto.

---
+++ backend proyecto/context/TestDbRH.cs
@@ -0,0 +1,23 @@
+﻿using backend_proyecto.model;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend_proyecto.context
+{
+    public class TestDbRH : DbContext
+    {
+        public TestDbRH (DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<User> users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>()
+                .HasKey(u => u.id);
+
+        }
+    }
+}
---
+++ backend proyecto/model/user.cs
@@ -0,0 +1,13 @@
+﻿namespace backend_proyecto.model
+{
+    public class User
+    {
+        public int id { get; set; }
+        public required string name { get; set; }
+
+        public required  string email { get; set; }
+        public required string password { get; set; }
+
+
+    }
+}
